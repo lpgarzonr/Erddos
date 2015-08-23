172843ffa298d6d2ef0fac64f9fc3b687b16d04e
leidy <guitarlady86@gmail.com>
2015-08-23 10:52:11 -0500

    Unit test for papers inicialization

--- app/index.js
+++ app/index.js
@@ -1,4 +1,21 @@
 function Author(firstName, lastName){
 	this.firstName = firstName;
 	this.lastName = lastName;
-}
+};
+
+function Paper(title, autors){
+	this.title = title;
+	this.authors = autors;
+};
+
+function buildInputPaperList(){
+	var papers = [
+		new Paper('Towards a component based GA', 
+		[new Author('Leidy P', 'Garzon'), new Author('Sergio', 'Rojas-Galeano'), new Author('Henry', 'Diosa')]),
+		new Paper('Estimation of relevant variables on high-dimensional biological patterns using iterated weighted kernel functions', 
+		[new Author('Sergio', 'Rojas-Galeano'), new Author('Dan', 'Agranoff'), new Author('Sanjeev', 'Krishna')]),
+		new Paper('The love of math', 
+		[new Author('Sergio', 'Rojas-Galeano'), new Author('Paul', 'Erdos'), new Author('Elisa', 'Rodriguez')])];
+	return papers;
+};
+
--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,49 @@
-QUnit.test( "hello test", function( assert ) {
-assert.ok( 1 == "1", "Passed!" );
+
+//TODO: All papers should have at least one outhor
+//TODO: At least one paper shour include Erdos as an author
+
+QUnit.module( "buildInputPaperArray test", {
+  beforeEach: function( assert ) {
+  	//arrange
+  	//act
+  	var papers = buildInputPaperList();
+  	//assert
+    assert.ok( papers.length > 0, "Non-empty paper list, success!");
+  }
 });
+QUnit.test( "all papers should have at least one author", function( assert ) {
+	//arrange
+	var papers = buildInputPaperList();
+
+	//act
+	var papersWithNoAuthors = papers.filter(function(paper){
+		return paper.authors.length == 0;
+	});
+
+	//assert
+	assert.equal( papersWithNoAuthors, 0 );
+});
+
+QUnit.test( "At least one paper should include Paul Erdos as an author", function( assert ) {
+	//arrange
+	var papers = buildInputPaperList();
+	var targetAuthor = new Author('Paul', 'Erdos');
+
+	//act
+	var authorsList = _.flatten(papers.map(function(paper){
+		return (paper.authors);
+	}));
+	var isErdosInAuthorList = containsAuthor(authorsList, targetAuthor);
+
+	//assert
+	assert.equal( isErdosInAuthorList, 1);
+});
+
+	function containsAuthor(arr, obj) {
+	    for (var i = 0; i < arr.length; i++) {
+	        if (arr[i].firstName == obj.firstName && arr[i].lastName == obj.lastName ) {
+	            return true;
+	        }
+	    }
+	    return false;
+	};
